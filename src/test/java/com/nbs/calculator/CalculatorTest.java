8960e8745127614998a7580e0ab81a66c4e9a577
rfinho <romans.filimonovs.other@gmail.com>
2019-06-24 15:30:07 +0100

    subtract feature

--- src/test/java/com/nbs/calculator/CalculatorTest.java
+++ src/test/java/com/nbs/calculator/CalculatorTest.java
@@ -5,12 +5,18 @@ import org.junit.Test;
 
 
 public class CalculatorTest {
-//hello
 	@Test
 	public void addTwoNumbersTogetherTest() {
 		Calculator calculator = new Calculator();
 		int result = calculator.add(2,2);
 		Assert.assertEquals(4, result);
+	}
+	
+	@Test
+	public void subtractTwoNumbersTogetherTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.subtract(4,2);
+		Assert.assertEquals(2, result);
 	}
 
 }
